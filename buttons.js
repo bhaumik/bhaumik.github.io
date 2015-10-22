783a1979fb0a4cc6986d2fa82b2f4f1b32cdfe1a
Bhaumik Patel <bhaumik@thinkful.com>
2015-10-21 22:33:24 -0400

    Refactor HTML/CSS to remove dead button

--- buttons.js
+++ buttons.js
@@ -1,6 +1,6 @@
 function shia() {
 	document.getElementById('button1').style.display = 'none';
 	document.getElementById('doit').style.visibility = 'visible';
-	document.getElementById('button2').style.visibility = 'visible';
+	document.getElementById('shuffle').style.visibility = 'visible';
 	return true;
 }
